ac39a674e8b36a70c4b3285e2ddf7fe720853f04
ralph <ralphmettle@gmail.com>
2024-07-06 16:36:21 +0100

    Fixed VirtualEnv

---
+++ lang_processing.py
@@ -0,0 +1,19 @@
+'''
+goals:
+get user location (lat, long)
+get user search input (e.g. scenic spot for a hike)
+process user search input via NLP
+extract keywords from user search input (e.g. 'scenic', 'hike') for search query in yelp API
+get yelp API response
+retrieve review count and review rating from yelp API response
+process top 3 yelp API results
+compare processed yelp API results with processed user search input
+score based on comparison to determine best matches
+recommend place to user
+'''
+
+def input_processing(user_input):
+    # NLP processing
+    # extract keywords from user input
+    # return keywords
+    pass
