32487b83ffaaba27ebc946578dcbe0226bef2ee6
Ralph <72173211+ralphmettle@users.noreply.github.com>
2024-08-14 16:30:25 +0000

    Added '.venv' to .gitignore and removed '.venv/'. Updated 'User' and 'Friendship' tables (to be tested). Added 'requirements.txt'.

--- models.py
+++ models.py
@@ -6,31 +6,25 @@ class User(db.Model, UserMixin):
     __tablename__ = 'user'
 
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-
+    
     username = db.Column(db.String, unique=True, nullable=False)
     email = db.Column(db.String, unique=True, nullable=False)
     password = db.Column(db.String, nullable=False)
     firstname = db.Column(db.String, default=None)
     lastname = db.Column(db.String, default=None)
     profile_picture = db.Column(db.String(500), default=None)
-
     role = db.Column(db.String, default='user')
     welcomed = db.Column(db.Boolean, default=False)
     verified = db.Column(db.Boolean, default=False)
-
     description = db.Column(db.String, default=None)
 
-    #friends = db.relationship('Friendship', backref='user', lazy=True) 
-    
-    sent_friend_requests = db.relationship('Friendship', 
-                                           foreign_keys='Friendship.sender_id', 
-                                           backref='sender', 
-                                           lazy=True)
-    
-    received_friend_requests = db.relationship('Friendship', 
-                                               foreign_keys='Friendship.recipient_id', 
-                                               backref='recipient', 
-                                               lazy=True)
+    friends = db.relationship(
+        'User',
+        secondary='friendship',
+        primaryjoin='and_(User.id==Friendship.user_id, Friendship.accepted==True)',
+        secondaryjoin='and_(User.id==Friendship.friend_id, Friendship.accepted==True)',
+        backref='friend_of'
+    )
     
     def __repr__(self): 
         return f'id: {self.id}; username: {self.username}; email: {self.email}; password: {self.password}; role: {self.role}; description: {self.description}'
@@ -39,13 +33,9 @@ class Friendship(db.Model):
     __tablename__ = 'friendship'
     
     id = db.Column(db.Integer, primary_key=True, unique=True, nullable=False)
-
-    sender_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    recipient_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    
+    user_id  = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    friend_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     accepted = db.Column(db.Boolean, default=False, nullable=False)
-    pending = db.Column(db.Boolean, default=True, nullable=False)
-
     date_created = db.Column(db.DateTime, default=db.func.now, nullable=False)
 
     def __repr__(self):
