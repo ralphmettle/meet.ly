3ce5ecbd086199dfb7d9e6a98a1c89215577dedd
Ralph <72173211+ralphmettle@users.noreply.github.com>
2025-02-07 13:10:10 +0000

    Update app.py
    Signed-off-by: Ralph <72173211+ralphmettle@users.noreply.github.com>

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 def debug_config():
     return True
